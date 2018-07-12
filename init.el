458d74321bfda6e894e655a7a99f6f4cc12ae9f0
Yonggang Wang <ygwang@thoguthworks.com>
2018-07-12 13:01:37 +0800

    add some package

--- init.el
+++ init.el
@@ -7,42 +7,20 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 
-(setq my/package-list
-      '(cider
-		clj-refactor
-		clojure-mode
-		company
-		exec-path-from-shell
-		flycheck
-		git
-		git-gutter
-		git-gutter-fringe
-		js2-mode
-		json-mode
-		json-reformat
-		magit
-		paredit
-		popup
-		projectile
-		rainbow-delimiters
-		rainbow-mode
-		smartparens
-		web-mode
-		yasnippet
-		use-package
-		))
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
 
-;; fetch the list of packages available
-(unless (file-exists-p package-user-dir)
-  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; install the missing packages
-(dolist (p my/package-list)
-  (when (not (package-installed-p p)) (package-install p)))
-
 (add-to-list 'load-path "~/.emacs.d/config")
-(require 'ui-conf)
-(require 'package-conf)
-(require 'org-conf)
+(require 'init-ui)
+(require 'init-blog)
+(require 'init-package)
