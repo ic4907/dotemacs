7210589fe4041b2ac45c16c37443e87ccfad5c26
wang yonggang <wangyonggang@wangs-MacBook-Pro.local>
2018-11-20 22:02:17 +0800

    refactory and add dot for org-mode

--- config/init-note.el
+++ config/init-note.el
@@ -4,9 +4,9 @@
 (use-package htmlize
   :ensure t)
 
-(setq org-mode-websrc-directory (concat (getenv "HOME") "/Library/Mobile Documents/com~apple~CloudDocs//notes/"))
-(setq org-mode-publishing-directory (concat (getenv "HOME") "/Public/notes"))
-;;(setq org-mode-publishing-directory "/ssh:root@orgdown.com:/var/www/blog")
+(setq org-project-base (concat (getenv "HOME") "/Documents/notes/"))
+(setq org-project-publish-base (concat (getenv "HOME") "/Public/notes"))
+;;(setq org-project-publish-base "/ssh:root@orgdown.com:/var/www/blog")
 
 (defun get-content
 	(x)
@@ -32,6 +32,10 @@
 	(setq org-src-fontify-natively t)
 	(setq org-startup-indented t)
 
+	(org-babel-do-load-languages
+	 'org-babel-load-languages
+	 '((dot . t)))
+
 	(setq org-use-fast-todo-selection t)
 
 	(setq org-todo-keywords
@@ -41,19 +45,19 @@
 								   ("DOING" . "yellow")
 								   ("DONE" . "green")))
 
-	(setq org-default-notes-file (concat org-mode-websrc-directory "inbox.org"))
+	(setq org-default-notes-file (concat org-mode-websrc-directory "gtd/inbox.org"))
 
-	(load-library "find-lisp")
-	(setq org-agenda-files (find-lisp-find-files org-mode-websrc-directory "\.org$"))
+	(setq org-agenda-files
+		  (directory-files-recursively (concat org-project-base "gtd") "\.org$"))
 	
 	(setq org-publish-project-alist
 		  `(
 			("note"
-			 :base-directory ,org-mode-websrc-directory
+			 :base-directory ,org-project-base
 			 :base-extension "org"
-			 :publishing-directory ,org-mode-publishing-directory
+			 :publishing-directory ,org-project-publish-base
 			 :publishing-function org-html-publish-to-html
-			 :with-toc nil
+			 ;;:with-toc nil
 			 :with-author t
 			 :with-email t
 			 :recursive t
@@ -62,16 +66,16 @@
 			 :email "shalir@outlook.com"
 			 :sitemap-filename "index.org"
 			 :html-doctype "html5"
-			 :exclude "thoughtworks/*"
+			 :exclude "\\(thoughtworks\\|gtd\\)/.*"
 			 :html-html5-fancy t
-			 :html-head  "<link rel=\"stylesheet\" href=\"/css/ic4907.css\" type=\"text/css\"/>"
-			 :html-preamble ,(get-content "templates/header.html")
-			 :html-postamble ,(get-content "templates/footer.html")
+			 ;;:html-head  "<link rel=\"stylesheet\" href=\"/css/ic4907.css\" type=\"text/css\"/>"
+			 ;;:html-preamble ,(get-content "templates/header.html")
+			 ;;:html-postamble ,(get-content "templates/footer.html")
 			 :html-head-include-default-style nil)
 			("note-static"
-			 :base-directory ,org-mode-websrc-directory
+			 :base-directory ,org-project-base
 			 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|svg"
-			 :publishing-directory ,org-mode-publishing-directory
+			 :publishing-directory ,org-project-publish-base
 			 :recursive t
 			 :publishing-function org-publish-attachment)
 
