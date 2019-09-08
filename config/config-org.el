66e121a3fd2a7003b670dd57f04d5a4c179cca18
ic4907 <shalir@outlook.com>
2019-09-08 22:07:26 +0800

    remove unsed org mode config

--- config/config-org.el
+++ config/config-org.el
@@ -26,30 +26,12 @@
          ("C-c b" . org-iswitchb))
   :config
   (progn
-	(use-package org-install)
-	(use-package ox)
-	(use-package org-archive)
-	(setq org-src-fontify-natively t)
-	(setq org-startup-indented t)
-	(setq org-export-with-drawers t)
-	(setq org-use-fast-todo-selection t)
-	;; (setq org-default-notes-file (concat org-project-base "gtd/inbox.org"))
-	;; (setq org-agenda-files
-	;;	  (directory-files-recursively (concat org-project-base "gtd") "\.org$"))
-
 	(org-babel-do-load-languages
 	 'org-babel-load-languages
 	 '((dot . t)
 	   (shell . t)))
 
-	(setq org-todo-keywords
-		  '((sequence "TODO(t)" "DOING(i)" "|" "DONE(d)" "ABORT(a)")))
-
-	(setq org-todo-keyword-faces '(("TODO" . "red")
-								   ("DOING" . "yellow")
-								   ("DONE" . "green")))
-
-	(setq org-html-divs '((preamble "header" "top")
+	(setq org-html-divs '((preamble "header" "preamble")
                       (content "main" "content")
                       (postamble "footer" "postamble"))
       org-html-container-element "section"
@@ -57,34 +39,39 @@
       org-html-checkbox-type 'html
       org-html-html5-fancy t
       org-html-htmlize-output-type 'css
-      org-html-head-include-default-style t
+      org-html-head-include-default-style nil
       org-html-head-include-scripts t
       org-html-doctype "html5"
       org-html-home/up-format "%s\n%s\n")
 
-	(setq orgweb-html-preamble "
-<h1 class=\"title\"><a href=\"/\">将大培的博客 </a>
-  <p class=\"subtitle\">何逊而今渐老，都忘却春风词笔。</p>
-</h1>
-<nav>
-  <a href=\"/\">About</a>
-  <a href=\"/cv\">CV</a>
-  <a href=\"/sitemap.html\">Blog</a>
-</nav>")
+	(setq orgweb-html-preamble
+	  "<div class=\"header\">
+         <div class=\"banner\">
+           <a href=\"/\">蒋大培的博客 </a>
+           <p class=\"subtitle\">何逊而今渐老，都忘却春风词笔。</p>
+         </div>
+         <nav class=\"nav\">
+           <a href=\"/\">About</a>
+           <a href=\"/cv.html\">CV</a>
+           <a href=\"/projects.html\">Project</a>
+           <a href=\"/articles/sitemap.html\">Blog</a>
+         </nav>
+       </div>")
 
-	(setq orgweb-html-postamble "<div>
-<p>Copyright 2019 by Jiang Dapei.
-Proudly published with <a href=\"https://www.gnu.org/software/emacs/\">Emacs</a> and <a href=\"https://orgmode.org/\">Org Mode</a>
-</div>")
+	(setq orgweb-html-postamble
+	  "<div class=\"site-info\">
+         <p class=\"copyright\">Jiang Dapei • 2019</p>
+         <p class=\"generator\">Proudly published with <a href=\"https://www.gnu.org/software/emacs/\">Emacs</a> and <a href=\"https://orgmode.org/\">Org Mode</a></p>
+      </div>")
+
+	(setq orgweb-head  "<link rel=\"stylesheet\" href=\"/css/style.css\" type=\"text/css\"/>")
 	
 	(setq org-publish-project-alist
 		  `(("home"
 			 :base-directory ,my-site-project-path
 			 :publishing-directory ,my-site-publish-path
 			 :publishing-function org-html-publish-to-html
-			 :html-doctype "html5"
-			 :html-html5-fancy t
-			 :html-head  "<link rel=\"stylesheet\" href=\"/css/ic4907.css\" type=\"text/css\"/>"
+			 :html-head  ,orgweb-head
 			 :html-preamble ,orgweb-html-preamble
 			 :html-postamble ,orgweb-html-postamble)
 			("articles"
@@ -92,29 +79,19 @@ Proudly published with <a href=\"https://www.gnu.org/software/emacs/\">Emacs</a>
 			 :base-extension "org"
 			 :publishing-directory ,(concat my-site-publish-path "articles")
 			 :publishing-function org-html-publish-to-html
-			 :with-author t
-			 :with-email t
-			 :with-creator t
 			 :recursive t
-			 :email "shalir@outlook.com"
 			 :auto-sitemap t
-			 :sitemap-filename "sitemap.org"
 			 :makeindex t
-			 :html-doctype "html5"
-			 :exclude "\\(thoughtworks\\|gtd\\)/.*"
-			 :html-html5-fancy t
-			 :html-head  "<link rel=\"stylesheet\" href=\"/css/ic4907.css\" type=\"text/css\"/>"
+			 :sitemap-title "Blog Posts"
+			 :html-head  ,orgweb-head
 			 :html-preamble ,orgweb-html-preamble
-			 :html-postamble ,orgweb-html-postamble
-			 :html-head-include-default-style nil)
+			 :html-postamble ,orgweb-html-postamble)
 			("static"
 			 :base-directory ,my-site-project-path
 			 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|svg"
 			 :publishing-directory ,my-site-publish-path
-			 :exclude "\\(thoughtworks\\|gtd\\)/.*"
 			 :recursive t
 			 :publishing-function org-publish-attachment)
-
 			("notebook" :components ("home" "articles" "static"))))))
 
 (provide 'config-org)
