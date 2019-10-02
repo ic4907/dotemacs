c1d0ee906682112efd752c839119b0d78d405b82
Jiang Dapei <ygwang@Jiangs-MBP.lan>
2019-10-02 22:14:33 +0800

    upate config for org mode

--- config/config-org.el
+++ config/config-org.el
@@ -6,8 +6,8 @@
 (use-package htmlize
   :ensure t)
 
-(setq my-site-project-path (concat (getenv "HOME") "/Documents/notes/"))
-(setq my-site-publish-path (concat (getenv "HOME") "/Public/notes/"))
+(setq blog-source-folder (concat (getenv "HOME") "/Documents/blog/"))
+(setq blog-target-folder (concat (getenv "HOME") "/Public/blog/"))
 
 (use-package org-bullets
   :ensure t
@@ -32,34 +32,33 @@
 	   (shell . t)))
 
 	(setq org-html-divs '((preamble "header" "preamble")
-                      (content "main" "content")
-                      (postamble "footer" "postamble"))
-      org-html-container-element "section"
-      org-html-metadata-timestamp-format "%Y-%m-%d"
-      org-html-checkbox-type 'html
-      org-html-html5-fancy t
-      org-html-htmlize-output-type 'css
-      org-html-head-include-default-style nil
-      org-html-head-include-scripts t
-      org-html-doctype "html5"
-      org-html-home/up-format "%s\n%s\n")
+						  (content "main" "content")
+						  (postamble "footer" "postamble"))
+		  org-html-container-element "section"
+		  org-html-metadata-timestamp-format "%Y-%m-%d"
+		  org-html-checkbox-type 'html
+		  org-html-html5-fancy t
+		  org-html-head-include-default-style nil
+		  org-html-head-include-scripts t
+		  org-html-doctype "html5"
+		  org-html-home/up-format "%s\n%s\n")
 
-	(setq orgweb-html-preamble
-	  "<div class=\"header\">
+	(setq blog-html-preamble
+		  "<div class=\"header\">
          <div class=\"banner\">
            <a href=\"/\">蒋大培的博客 </a>
-           <p class=\"subtitle\">何逊而今渐老，都忘却春风词笔。</p>
+           <p class=\"subtitle\">爱技术，喜欢计算机编程。</p>
          </div>
          <nav class=\"nav\">
            <a href=\"/\">About</a>
            <a href=\"/cv.html\">CV</a>
-           <a href=\"/projects.html\">Project</a>
+           <a href=\"/projects/\">Project</a>
            <a href=\"/articles/sitemap.html\">Blog</a>
          </nav>
        </div>")
 
-	(setq orgweb-html-postamble
-	  "<div class=\"site-info\">
+	(setq blog-html-postamble
+		  "<div class=\"site-info\">
          <p class=\"copyright\">Jiang Dapei • 2019</p>
          <p class=\"generator\">Proudly published with <a href=\"https://www.gnu.org/software/emacs/\">Emacs</a> and <a href=\"https://orgmode.org/\">Org Mode</a></p>
 <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -73,34 +72,44 @@
 </script>
       </div>")
 
-	(setq orgweb-head  "<link rel=\"stylesheet\" href=\"/css/style.css\" type=\"text/css\"/>")
-	
+	(setq blog-head  "<link rel=\"stylesheet\" href=\"/css/style.css\" type=\"text/css\"/>")
+
 	(setq org-publish-project-alist
-		  `(("home"
-			 :base-directory ,my-site-project-path
-			 :publishing-directory ,my-site-publish-path
+		  `(("page"
+			 :base-directory ,blog-source-folder
+			 :publishing-directory ,blog-target-folder
 			 :publishing-function org-html-publish-to-html
-			 :html-head  ,orgweb-head
-			 :html-preamble ,orgweb-html-preamble
-			 :html-postamble ,orgweb-html-postamble)
-			("articles"
-			 :base-directory ,(concat my-site-project-path "articles")
+			 :html-head  ,blog-head
+			 :html-preamble ,blog-html-preamble
+			 :html-postamble ,blog-html-postamble)
+			("projects"
+			 :base-directory ,(concat blog-source-folder "projects")
+			 :publishing-directory ,(concat blog-target-folder "projects")
 			 :base-extension "org"
-			 :publishing-directory ,(concat my-site-publish-path "articles")
-			 :publishing-function org-html-publish-to-html
 			 :recursive t
+			 :publishing-function org-html-publish-to-html
+			 :html-head  ,blog-head
+			 :html-preamble ,blog-html-preamble
+			 :html-postamble ,blog-html-postamble)
+			("articles"
+			 :base-directory ,(concat blog-source-folder "articles")
+			 :publishing-directory ,(concat blog-target-folder "articles")
+			 :base-extension "org"
+			 :recursive t
+			 :publishing-function org-html-publish-to-html
 			 :auto-sitemap t
 			 :makeindex t
-			 :sitemap-title "Blog Posts"
-			 :html-head  ,orgweb-head
-			 :html-preamble ,orgweb-html-preamble
-			 :html-postamble ,orgweb-html-postamble)
-			("static"
-			 :base-directory ,my-site-project-path
+			 :sitemap-title ""
+			 :html-head  ,blog-head
+			 :html-preamble ,blog-html-preamble
+			 :html-postamble ,blog-html-postamble)
+			("resources"
+			 :base-directory ,blog-source-folder
+			 :publishing-directory ,blog-target-folder
 			 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|svg"
-			 :publishing-directory ,my-site-publish-path
 			 :recursive t
 			 :publishing-function org-publish-attachment)
-			("notebook" :components ("home" "articles" "static"))))))
+			("blog" :components ("page" "projects" "articles" "resources"))))))
 
 (provide 'config-org)
+
